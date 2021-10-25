7c19c133f44358506c17394b4ebfac49f584c81d
jeromp <jerom.pathipat@trivadis.com>
2021-10-25 14:44:16 +0200

    Add basic Document Search

--- persistence/src/main/java/com/github/jeromp/documentmanagementsystem/persistence/repository/DocumentRepository.java
+++ persistence/src/main/java/com/github/jeromp/documentmanagementsystem/persistence/repository/DocumentRepository.java
@@ -2,12 +2,24 @@ package com.github.jeromp.documentmanagementsystem.persistence.repository;
 
 import com.github.jeromp.documentmanagementsystem.persistence.model.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface DocumentRepository extends JpaRepository<Document, Long> {
     Optional<Document> findByUuid(UUID uuid);
+
+    @Query("SELECT d, m FROM Document d JOIN d.meta m where " +
+            ":title is null or d.title = :title and " +
+            ":description is null or m.description = :description")
+    List<Document> findByQuery(@Param("title") String title,
+                               @Param("description") String description /*,
+                               @Param("documentCreatedAfter") Date documentCreatedAfter,
+                               @Param("documentCreatedBefore") Date documentCreatedBefore*/);
 }
--- persistence/src/test/java/com/github/jeromp/documentmanagementsystem/persistence/repository/DocumentRepositoryTest.java
+++ persistence/src/test/java/com/github/jeromp/documentmanagementsystem/persistence/repository/DocumentRepositoryTest.java
@@ -1,7 +1,10 @@
 package com.github.jeromp.documentmanagementsystem.persistence.repository;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.UUID;
 
+import com.github.jeromp.documentmanagementsystem.persistence.model.Meta;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
@@ -24,11 +27,14 @@ class DocumentRepositoryTest {
     private static final String DOCUMENT_TITLE = "Test Document";
     private static final String DOCUMENT_PATH = "test_path";
     private static final UUID DOCUMENT_UUID = UUID.randomUUID();
+    private static final String META_DESCRIPTION = "This is a description.";
+    private static final LocalDateTime YESTERDAY_DATE = LocalDateTime.now().minusDays(1);
 
     @Autowired
     private DocumentRepository repository;
 
     private Document persistedDocUnderTest;
+    private Meta persistedMetaUnderTest;
 
     @BeforeEach
     void setUp() {
@@ -36,6 +42,11 @@ class DocumentRepositoryTest {
         this.persistedDocUnderTest.setUuid(DOCUMENT_UUID);
         this.persistedDocUnderTest.setTitle(this.DOCUMENT_TITLE);
         this.persistedDocUnderTest.setPath(this.DOCUMENT_PATH);
+        this.persistedMetaUnderTest = new Meta();
+        this.persistedMetaUnderTest.setDescription(META_DESCRIPTION);
+        this.persistedMetaUnderTest.setDocumentCreated(YESTERDAY_DATE);
+        this.persistedMetaUnderTest.setDocument(this.persistedDocUnderTest);
+        this.persistedDocUnderTest.setMeta(this.persistedMetaUnderTest);
         assertNotNull(this.persistedDocUnderTest = this.repository.save(persistedDocUnderTest));
     }
 
@@ -47,7 +58,7 @@ class DocumentRepositoryTest {
 
     @Test
     @DisplayName("Test if inserted Document is complete")
-    void findById(){
+    void findById() {
         var document2 = this.repository.findByUuid(this.persistedDocUnderTest.getUuid()).orElseThrow();
         assertAll("all properties",
                 () -> assertEquals(persistedDocUnderTest.getId(), document2.getId()),
@@ -58,8 +69,42 @@ class DocumentRepositoryTest {
         );
     }
 
+    @Test
+    @DisplayName("Get Documents by Query with title")
+    void findByQueryWithTitle() {
+        var documentList = this.repository.findByQuery(DOCUMENT_TITLE, null /*, null, null*/);
+        assertAll("",
+                () -> assertEquals(1, documentList.size()),
+                () -> assertEquals(DOCUMENT_TITLE, documentList.get(0).getTitle())
+        );
+    }
+
+    @Test
+    @DisplayName("Get Documents by Query with title and description")
+    void findByQueryWithTitleAndDescription() {
+        var documentList = this.repository.findByQuery(DOCUMENT_TITLE, META_DESCRIPTION /*, null, null*/);
+        assertAll("",
+                () -> assertEquals(1, documentList.size()),
+                () -> assertEquals(DOCUMENT_TITLE, documentList.get(0).getTitle()),
+                () -> assertEquals(META_DESCRIPTION, documentList.get(0).getMeta().getDescription())
+        );
+    }
+
+    @Test
+    @DisplayName("Get Documents by Query with date parameters")
+    void findByQueryWithDate() {
+
+    }
+
+    @Test
+    @DisplayName("Get Empty List of Documents after Query")
+    void findEmptyListByQuery() {
+        var documentList = this.repository.findByQuery("WRONG_TITLE", null /*, null, null*/);
+        assertEquals(0, documentList.size());
+    }
+
     @AfterEach
-    void tearDown(){
+    void tearDown() {
         assertDoesNotThrow(() -> this.repository.delete(this.persistedDocUnderTest));
     }
 
